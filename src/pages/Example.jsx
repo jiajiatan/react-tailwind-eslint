f0d6be3a9c38122911587568ed7aa283bcf5e4d5
Tan Jia Jia <71870881+jiajiatan@users.noreply.github.com>
2023-02-20 21:54:56 +0800

    minor edit: added btn, change theme

--- src/pages/Example.jsx
+++ src/pages/Example.jsx
@@ -5,6 +5,7 @@ const Example = () => {
     <div className="bg-gray-200 rounded-xl shadow border p-8 m-10">
       <p className="text-3xl text-gray-700 font-bold mb-5">Welcome!</p>
       <p className="text-gray-500 text-lg">React and Tailwind CSS in action</p>
+      <button className="btn btn-primary mt-4">Button</button>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,10 @@ module.exports = {
     themes: [
       {
         mytheme: {
-          primary: '#D1C1D7',
-          secondary: '#F6CBD1',
-          accent: '#B4E9D6',
-          neutral: '#70ACC7',
+          primary: '#570DF8',
+          secondary: '#F000B8',
+          accent: '#37CDBE',
+          neutral: '#3D4451',
           'base-100': '#FFFFFF',
           info: '#3ABFF8',
           success: '#36D399',
